87023eb1aa598c8e394e2f329d36b19e16d551c5
hau <hau@QUALITYA17>
2017-10-19 20:11:45 -0700

    Add project files.

---
+++ NerdDinnerTC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NerdDinnerTC.MvcApplication" Language="C#" %>
---
+++ NerdDinnerTC/Models/Dinner.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Web;
+
+namespace NerdDinnerTC.Models
+{
+    public partial class Dinner
+    {
+        public bool IsValid
+        {
+            get { return (GetRuleViolations().Count() == 0); }
+        }
+
+        public IEnumerable<RuleViolation> GetRuleViolations()
+        {
+            if (String.IsNullOrEmpty(Title))
+                yield return new RuleViolation("Title required", "Title");
+
+            if (String.IsNullOrEmpty(Description))
+                yield return new RuleViolation("Description required", "Description");
+
+            if (String.IsNullOrEmpty(HostedBy))
+                yield return new RuleViolation("Hosted By reqired", "HostedBy");
+
+            if (String.IsNullOrEmpty(Address))
+                yield return new RuleViolation("Address required", "Address");
+
+            if (String.IsNullOrEmpty(Country))
+                yield return new RuleViolation("Country required", "Country");
+
+            if (String.IsNullOrEmpty(ContactPhone))
+                yield return new RuleViolation("Phone# required", "ContactPhone");
+
+            //if (!PhoneValidator.IsValidNumber(ContactPhone, Country))
+                //yield return new RuleViolation("Phone# does not match country", "Contact Phone");
+
+            
+                    yield break;
+        }
+
+        partial void OnValidate(ChangeAction action)
+        {
+            if (!IsValid)
+                throw new ApplicationException("Rule violations prevent saving");
+        }
+    }
+
+    public class RuleViolation
+    {
+        public string ErrorMessage { get; private set; }
+        public string PropertyName { get; private set; }
+
+        public RuleViolation(string errorMessage, string propertyName)
+        {
+            ErrorMessage = errorMessage;
+            PropertyName = propertyName;
+        }
+    }
+}
